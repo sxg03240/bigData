86c0c43ad4b7218db2c7dc34743f9e3b417370f2
sxg03240 <112096651+sxg03240@users.noreply.github.com>
2023-03-09 16:00:55 -0600

    git commit

---
+++ mapReduce/src/main/java/LetterFrequencyCount.java
@@ -0,0 +1,62 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class LetterFrequencyCount {
+    public static class LetterMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+        private final static IntWritable one = new IntWritable(1);
+        private Text letter = new Text();
+
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString().toLowerCase();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                String word = tokenizer.nextToken();
+                for (int i = 0; i < word.length(); i++) {
+                    char ch = word.charAt(i);
+                    if (Character.isLetter(ch)) {
+                        letter.set(String.valueOf(ch));
+                        context.write(letter, one);
+                    }
+                }
+            }
+        }
+    }
+
+    public static class LetterReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "letter count");
+        job.setJarByClass(LetterFrequencyCount.class);
+        job.setMapperClass(LetterMapper.class);
+        job.setCombinerClass(LetterReducer.class);
+        job.setReducerClass(LetterReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
+
